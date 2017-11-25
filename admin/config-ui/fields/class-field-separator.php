fb49cedffc06ee8557ded422b2cfac0cf8390bc1
gelther <lpw.sormano@gmail.com>
2017-11-25 10:21:33 +0100

    Code consistency changes for admin/config-ui/fields/class-field-separator.php

--- admin/config-ui/fields/class-field-separator.php
+++ admin/config-ui/fields/class-field-separator.php
@@ -40,4 +40,5 @@ class WPSEO_Config_Field_Separator extends WPSEO_Config_Field_Choice {
 	public function set_adapter( WPSEO_Configuration_Options_Adapter $adapter ) {
 		$adapter->add_yoast_lookup( $this->get_identifier(), 'wpseo_titles', 'separator' );
 	}
+
 }
