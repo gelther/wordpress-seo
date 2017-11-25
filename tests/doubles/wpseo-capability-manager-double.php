ed4e42939cfc4155587c5c4a98904f86c5de89df
gelther <lpw.sormano@gmail.com>
2017-11-25 10:30:17 +0100

    Code consistency changes for tests/doubles/wpseo-capability-manager-double.php

--- tests/doubles/wpseo-capability-manager-double.php
+++ tests/doubles/wpseo-capability-manager-double.php
@@ -11,14 +11,12 @@ class WPSEO_Capability_Manager_Double extends WPSEO_Abstract_Capability_Manager 
 	 * Adds the registered capabilities to the system.
 	 */
 	public function add() {
-
 	}
 
 	/**
 	 * Removes the registered capabilities from the system
 	 */
 	public function remove() {
-
 	}
 
 	public function get_registered_capabilities() {
@@ -28,4 +26,5 @@ class WPSEO_Capability_Manager_Double extends WPSEO_Abstract_Capability_Manager 
 	public function filter_roles( $capability, array $roles ) {
 		return parent::filter_roles( $capability, $roles );
 	}
+
 }
