1fb4f6669d1afe19cbd04a0fc29c29f671e06bef
gelther <lpw.sormano@gmail.com>
2017-11-25 10:21:14 +0100

    Code consistency changes for admin/config-ui/fields/class-field-post-type-visibility.php

--- admin/config-ui/fields/class-field-post-type-visibility.php
+++ admin/config-ui/fields/class-field-post-type-visibility.php
@@ -15,14 +15,15 @@ class WPSEO_Config_Field_Post_Type_Visibility extends WPSEO_Config_Field {
 		parent::__construct( 'postTypeVisibility', 'HTML' );
 
 		$copy = __( 'Please specify which post types you want search engines to index.
- If you do not know the differences between these, it\'s best to choose the
- default settings. For instance, setting \'Media\' to visible would index your
- images which could lead to visitors finding your images without ever
- coming to your site. If you have a custom post type on your site, you need
- to activate these manually.', 'wordpress-seo' );
+	If you do not know the differences between these, it\'s best to choose the
+	default settings. For instance, setting \'Media\' to visible would index your
+	images which could lead to visitors finding your images without ever
+	coming to your site. If you have a custom post type on your site, you need
+	to activate these manually.', 'wordpress-seo' );
 
 		$html = '<p>' . esc_html( $copy ) . '</p><br/>';
 
 		$this->set_property( 'html', $html );
 	}
+
 }
