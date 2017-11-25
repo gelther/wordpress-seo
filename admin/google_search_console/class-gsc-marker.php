9778f3454d5ba828fed51b4365df5b83a4a46344
gelther <lpw.sormano@gmail.com>
2017-11-25 10:22:28 +0100

    Code consistency changes for admin/google_search_console/class-gsc-marker.php

--- admin/google_search_console/class-gsc-marker.php
+++ admin/google_search_console/class-gsc-marker.php
@@ -106,7 +106,7 @@ class WPSEO_GSC_Marker {
 	/**
 	 * Sending a request to the Google Search Console API to let them know we marked an issue as fixed.
 	 *
-	 * @param WPSEO_GSC_Service $service Service object instance.
+	 * @param  WPSEO_GSC_Service $service Service object instance.
 	 *
 	 * @return bool
 	 */
@@ -140,4 +140,5 @@ class WPSEO_GSC_Marker {
 		// And update the count.
 		$counts->update_issue_count( $this->platform, $this->category, $total_issues );
 	}
+
 }
